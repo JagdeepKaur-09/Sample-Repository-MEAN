7dd6bc2c32302a912cbb3a8d1bf2119a661b733c
Jagdeep Kaur <sghps05jagdeep10a@gmail.com>
2026-03-31 00:02:06 +0530

    get room by code

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,18 +1,29 @@
-const Room = require("../models/Room");
 const express = require("express");
 const router = express.Router();
+const Room = require("../models/Room");
 const crypto = require("crypto");
 
 // Create a room
 router.post("/create", async (req, res) => {
   const { eventName, organizerId } = req.body;
   try {
-    const roomCode = crypto.randomBytes(6).toString("hex"); // generates random room code
+    const roomCode = crypto.randomBytes(6).toString("hex");
     const room = new Room({ eventName, organizerId, roomCode });
     await room.save();
     res.json({ message: "Room created!", roomCode, roomLink: `http://localhost:5000/room/${roomCode}` });
   } catch (err) {
     res.status(400).json({ error: err.message });
+  }
+});
+
+// Get room by code
+router.get("/:roomCode", async (req, res) => {
+  try {
+    const room = await Room.findOne({ roomCode: req.params.roomCode });
+    if (!room) return res.status(404).json({ error: "Room not found" });
+    res.json(room);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 });
 
